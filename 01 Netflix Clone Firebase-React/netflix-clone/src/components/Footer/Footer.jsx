dce9f34e01cfa82f3aa88a5bac11c5a6164c8135
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-11-21 20:14:46 +0530

    code

--- 01 Netflix Clone Firebase-React/netflix-clone/src/components/Footer/Footer.jsx
+++ 01 Netflix Clone Firebase-React/netflix-clone/src/components/Footer/Footer.jsx
@@ -14,6 +14,21 @@ const Footer = () => {
         <img src={twitter_icon} alt="" />
         <img src={youtube_icon} alt="" />
       </div>
+      <ul>
+        <li>Audio Description</li>
+        <li>Help Centre</li>
+        <li>Gift Cards</li>
+        <li>Media Centre</li>
+        <li>Investor Relations</li>
+        <li>Jobs</li>
+        <li>Terms of Use</li>
+        <li>Privacy</li>
+        <li>Legal Notices</li>
+        <li>Cookies Prefeneces</li>
+        <li>Corporat Information</li>
+        <li>Contact Us</li>
+      </ul>
+      <p className='copyright-text'>@ 2001-2024 Netflix by Sethi Tech Pvt. Ltd.</p>
     </div>
   )
 }
