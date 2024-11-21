1675ce572d54844aaa67a97a75524396b429a428
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-11-21 10:05:24 +0530

    code

--- 01 Netflix Clone Firebase-React/netflix-clone/src/components/TitleCards/TitleCards.jsx
+++ 01 Netflix Clone Firebase-React/netflix-clone/src/components/TitleCards/TitleCards.jsx
@@ -1,12 +1,23 @@
-import React from 'react'
-import "./TitleCards.css"
+import React from "react";
+import "./TitleCards.css";
+import card_data from "../../assets/cards/Cards_data";
 
 const TitleCards = () => {
   return (
     <div className="titlecards">
-      
+      <h2>Popular on Netflix</h2>
+      <div className="card-list">
+        {card_data.map((card, index) => {
+          return (
+            <div className="card" key={index}>
+              <img src={card.image} alt="" />
+              <p>{card.name}</p>
+            </div>
+          );
+        })}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default TitleCards
+export default TitleCards;
