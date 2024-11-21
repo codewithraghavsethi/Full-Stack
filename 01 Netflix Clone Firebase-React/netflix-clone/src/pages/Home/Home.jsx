d4ed7c27b7ef1c0aedadf419f5565977a5858a64
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-11-21 08:42:38 +0530

    code

--- 01 Netflix Clone Firebase-React/netflix-clone/src/pages/Home/Home.jsx
+++ 01 Netflix Clone Firebase-React/netflix-clone/src/pages/Home/Home.jsx
@@ -1,11 +1,20 @@
 import React from "react";
 import "./Home.css";
 import Navbar from "../../components/Navbar/Navbar.jsx";
+import hero_banner from "../../assets/background_banner.jpg"
+import hero_title from "../../assets/hero_title.png"
 
 const Home = () => {
   return (
     <div className="home">
       <Navbar />
+      <div className="hero">
+        <img src={hero_banner} alt="" className="banner-img"/>
+        <div className="hero-caption">
+          <img src={hero_title} alt="" className="caption-img" />
+          <p>Discovering his ties to a secret ancient order, a young man living in modern instanbul embark on a quest to save the city from the immortal enemy.</p>
+        </div>
+      </div>
     </div>
   );
 };
