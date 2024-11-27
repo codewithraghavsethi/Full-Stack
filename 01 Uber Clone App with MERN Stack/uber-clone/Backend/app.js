95834848c847688975222beb3dc9f931719421d7
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-11-27 10:33:29 +0530

    code

--- 01 Uber Clone App with MERN Stack/uber-clone/Backend/app.js
+++ 01 Uber Clone App with MERN Stack/uber-clone/Backend/app.js
@@ -1,4 +1,17 @@
+const dotenv = require('dotenv')
+dotenv.config()
 const express = require("express");
 const app = express();
+const cors = require('cors');
+const connectToDb = require('./db/db')
 
-app.get("/", (req, res) => {});
+
+connectToDb()
+
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("Hello World");
+});
+
+module.exports = app;
