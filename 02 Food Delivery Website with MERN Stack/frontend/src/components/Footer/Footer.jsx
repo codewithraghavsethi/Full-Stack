302ab5b772042b07cdb1bd58240529a027ab6f71
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-12-01 17:48:09 +0530

    code

--- 02 Food Delivery Website with MERN Stack/frontend/src/components/Footer/Footer.jsx
+++ 02 Food Delivery Website with MERN Stack/frontend/src/components/Footer/Footer.jsx
@@ -38,7 +38,7 @@ const Footer = () => {
       </div>
       <hr />
       <p className="footer-copyright">
-        Copyright 2025 @ Tomato.com - All Right Reserved.
+        Copyright 2025 @Tomato.com - All Right Reserved.
       </p>
     </div>
   );
