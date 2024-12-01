3db43b5b2b3d4cf964953718fc03454705552bbd
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-12-01 20:25:31 +0530

    code

--- 02 Food Delivery Website with MERN Stack/frontend/src/components/LoginPopup/LoginPopup.jsx
+++ 02 Food Delivery Website with MERN Stack/frontend/src/components/LoginPopup/LoginPopup.jsx
@@ -1,12 +1,48 @@
-import React from 'react'
-import "./LoginPopup.css"
+import React from "react";
+import "./LoginPopup.css";
+import { useState } from "react";
+import { assets } from "../../assets/frontend_assets/assets";
 
-const LoginPopup = () => {
+const LoginPopup = ({ setShowLogin }) => {
+  const [currState, setCurrState] = useState("Login Up");
+
   return (
-    <div className='login-popup'>
-      Login 
+    <div className="login-popup">
+      <form className="login-popup-container">
+        <div className="login-popup-title">
+          <h2>{currState}</h2>
+          <img
+            onClick={() => setShowLogin(false)}
+            src={assets.cross_icon}
+            alt=""
+          />
+        </div>
+        <div className="login-popup-inputs">
+          {currState === "Login" ? (
+            <></>
+          ) : (
+            <input type="text" placeholder="Your name" required />
+          )}
+          <input type="email" placeholder="Your email" required />
+          <input type="password" placeholder="Password" required />
+        </div>
+        <button>{currState === "Sign Up" ? "Create account" : "Login"}</button>
+        <div className="login-popup-condition">
+          <input type="checkbox" required />
+          <p>By Continuing, i agree to the terms of use & privacy policy.</p>
+        </div>
+        {currState === "Login" ? (
+          <p>
+            Create a New Account? <span onClick={()=>setCurrState("Sign Up")}>Click here!</span>
+          </p>
+        ) : (
+          <p>
+            Alredy have an Account? <span onClick={()=>setCurrState("Login")}>Login here!</span>
+          </p>
+        )}
+      </form>
     </div>
-  )
-}
+  );
+};
 
-export default LoginPopup
+export default LoginPopup;
