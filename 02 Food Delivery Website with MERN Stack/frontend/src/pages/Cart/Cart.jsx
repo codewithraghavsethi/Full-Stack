348c773e1a941f1c74d753b811854e9e41b1cd5f
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-12-02 10:19:33 +0530

    code

--- 02 Food Delivery Website with MERN Stack/frontend/src/pages/Cart/Cart.jsx
+++ 02 Food Delivery Website with MERN Stack/frontend/src/pages/Cart/Cart.jsx
@@ -1,12 +1,27 @@
-import React from 'react'
-import "./Cart.css"
+import React from "react";
+import { useContext } from "react";
+import "./Cart.css";
+import StoreContext from "../../components/Context/StoreContext";
 
 const Cart = () => {
-  return (
-    <div>
-      
-    </div>
-  )
-}
+  const { cartItems, food_list, removeFromCart } = useContext(StoreContext);
 
-export default Cart
+  return (
+    <div className="cart">
+      <div className="cart-items">
+        <div className="cart-items-title">
+          <p>Items</p>
+          <p>Title</p>
+          <p>Price</p>
+          <p>Quantity</p>
+          <p>Total</p>
+          <p>Remove</p>
+        </div>
+        <br />
+        <hr />
+      </div>
+    </div>
+  );
+};
+
+export default Cart;
