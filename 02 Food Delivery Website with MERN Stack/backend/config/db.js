26b84fc27d9075e95e9e6b9d136ebd4c34632b89
codewithraghavsethi <codewithraghavsethi@gmail.com>
2024-12-05 14:16:37 +0530

    code

--- 02 Food Delivery Website with MERN Stack/backend/config/db.js
+++ 02 Food Delivery Website with MERN Stack/backend/config/db.js
@@ -1,12 +1,8 @@
 import mongoose from "mongoose";
 
 export const connectDB = async () => {
-    await mongoose.connect('mongodb://127.0.0.1:27017/food-del').then(()=>console.log("connected"))
-}
-
-
-
-
-
-
+  await mongoose
+    .connect("mongodb://0.0.0.0")
+    .then(() => console.log("connected! Hello MongoDB"));
+};
 
