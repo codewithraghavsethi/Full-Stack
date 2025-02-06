dfab5fe8c2fd821149ddd7a415332626ff3c0fc8
codewithraghavsethi <codewithraghavsethi@gmail.com>
2025-02-06 22:20:40 +0530

    code

--- 01 Food Delivery Website with MERN Stack/backend/config/db.js
+++ 01 Food Delivery Website with MERN Stack/backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from "mongoose";
 
 export const connectDB = async () => {
   await mongoose
-    .connect("mongodb+srv://raghav:raghav123@cluster0.p8ncv.mongodb.net/food-del-sethitech")
+    .connect("mongodb+srv://raghav:raghav123@cluster0.p8ncv.mongodb.net/food-del")
     .then(() => console.log("connected! Hello MongoDB"));
 };
 
