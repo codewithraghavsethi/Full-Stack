043350cd488fdb24727383af6eacab4d97ad334d
codewithraghavsethi <codewithraghavsethi@gmail.com>
2025-02-13 11:03:58 +0530

    code

--- 04 LMS - Learning Management System Website/client/src/App.jsx
+++ 04 LMS - Learning Management System Website/client/src/App.jsx
@@ -3,10 +3,17 @@ import React from 'react'
 const App = () => {
   return (
     <div>
-      <h1>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ut, laboriosam! Officiis delectus eaque quam nesciunt molestias reiciendis dolore quae quas, qui nihil. Voluptatem nisi quae ex ratione, eaque natus libero?</h1>
+      <h1>Hello Raghav Sethi</h1>
     </div>
   )
 }
 
 export default App
+
+
+
+
+
+
+
 
