a4d3c2ef8ea6e4673edbe4d574e8de5f18833278
codewithraghavsethi <codewithraghavsethi@gmail.com>
2025-02-15 20:03:35 +0530

    code

--- 04 LMS - Learning Management System Website/client/src/App.jsx
+++ 04 LMS - Learning Management System Website/client/src/App.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React from "react";
+import { Route, Routes } from "react-router-dom";
+import Home from "./pages/student/Home";
+import CoursesList from "./pages/student/CoursesList";
+import CourseDetails from "./pages/student/CourseDetails";
+import MyEnrollments from "./pages/student/MyEnrollments";
+import Player from "./pages/student/Player";
 
 const App = () => {
   return (
     <div>
-      <h1>Hello Raghav Sethi</h1>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/course-list" element={<CoursesList />} />
+        <Route path="/course-list/:input" element={<CoursesList />} />
+        <Route path="/course/:id" element={<CourseDetails />} />
+        <Route path="/my-enrollments" element={<MyEnrollments />} />
+        <Route path="/player/:courseId" element={<Player />} />
+      </Routes>
     </div>
-  )
-}
+  );
+};
 
-export default App
-
-
-
-
-
-
-
-
+export default App;
--- 04 LMS - Learning Management System Website/client/src/context/AppContext.jsx
+++ 04 LMS - Learning Management System Website/client/src/context/AppContext.jsx
@@ -11,3 +11,4 @@ export const AppContextProvider = (props) => {
     </AppContext.Provider>
   );
 };
+
