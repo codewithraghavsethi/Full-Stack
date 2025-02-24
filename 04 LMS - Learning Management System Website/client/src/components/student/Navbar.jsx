18ce226c6aeac27bb9e3dec86675e53ca05d5102
codewithraghavsethi <codewithraghavsethi@gmail.com>
2025-02-24 20:29:35 +0530

    code

--- 04 LMS - Learning Management System Website/client/src/components/student/Navbar.jsx
+++ 04 LMS - Learning Management System Website/client/src/components/student/Navbar.jsx
@@ -3,8 +3,14 @@ import { assets } from "../../assets/assets";
 import { Link } from "react-router-dom";
 
 const Navbar = () => {
+  const isCourseListPage = location.pathname.includes("/");
+
   return (
-    <div className="flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4">
+    <div
+      className={` flex items-center justify-between px-4 sm:px-10 md:px-14 lg:px-36 border-b border-gray-500 py-4 ${
+        isCourseListPage ? "bg-white" : "bg-cyan-100/70"
+      }`}
+    >
       <img
         src={assets.logo}
         alt="Logo"
