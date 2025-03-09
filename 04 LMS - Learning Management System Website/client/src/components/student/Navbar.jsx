514f52b48c8808fc138b1f5b403a88e609bb383f
codewithraghavsethi <codewithraghavsethi@gmail.com>
2025-03-09 21:33:46 +0530

    code

--- 04 LMS - Learning Management System Website/client/src/components/student/Navbar.jsx
+++ 04 LMS - Learning Management System Website/client/src/components/student/Navbar.jsx
@@ -6,7 +6,7 @@ import { AppContext } from "../../context/AppContext";
 
 const Navbar = () => {
 
-  const { navigate } = useContext(AppContext)
+  const { navigate, isEducator } = useContext(AppContext)
 
   const isCourseListPage = location.pathname.includes("/course-list");
 
@@ -21,7 +21,7 @@ const Navbar = () => {
       <div className="hidden md:flex items-center gap-5 text-gray-500">
         <div className="flex items-center gap-5 ">
           { user && <> 
-            <button>{}</button>
+            <button onClick={()=>{navigate("/educator")}}>{isEducator ? "Educator Dashbord" : "Become Educator"}</button>
           |<Link to="/my-enrollments">My Enrollments</Link>
           </>
           }
