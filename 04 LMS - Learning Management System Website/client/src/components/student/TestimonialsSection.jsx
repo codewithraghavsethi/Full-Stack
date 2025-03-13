c1db77423e464322542a1c5d7ffb85a408d38dac
codewithraghavsethi <codewithraghavsethi@gmail.com>
2025-03-13 18:41:52 +0530

    code

--- 04 LMS - Learning Management System Website/client/src/components/student/TestimonialsSection.jsx
+++ 04 LMS - Learning Management System Website/client/src/components/student/TestimonialsSection.jsx
@@ -2,7 +2,10 @@ import React from 'react'
 
 const TestimonialsSection = () => {
   return (
-    <div>TestimonialsSection</div>
+    <div className='pb-14 '>
+      <h2>Testimonials</h2>
+      <p>Hear from our learners as they share their journeys of transformation, success, and how our <br /> platform has made a difference in their lives.</p>
+    </div>
   )
 }
 
