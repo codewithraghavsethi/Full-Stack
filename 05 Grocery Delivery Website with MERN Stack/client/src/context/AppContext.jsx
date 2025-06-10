bf6447287dd17358fa1759daaa077786b57edfab
codewithraghavsethi <codewithraghavsethi@gmail.com>
2025-06-10 19:04:41 +0530

    code

--- 05 Grocery Delivery Website with MERN Stack/client/src/context/AppContext.jsx
+++ 05 Grocery Delivery Website with MERN Stack/client/src/context/AppContext.jsx
@@ -1,13 +1,24 @@
-import { createContext, useContext } from "react";
+import { createContext, useContext, useState } from "react";
+import { useNavigate } from "react-router-dom";
 
 export const AppContext = createContext();
 
 export const AppContextProvider = ({ children }) => {
-  const value = {};
+  const navigate = useNavigate();
+  const [user, setUser] = useState(null);
+  const [isSeller, setIsSeller] = useState(false);
+
+  const value = {
+    navigate,
+    user,
+    setUser,
+    isSeller,
+    setIsSeller,
+  };
+
   return <AppContext.Provider value={value}>{children}</AppContext.Provider>;
 };
 
 export const useAppContext = () => {
   return useContext(AppContext);
 };
-
