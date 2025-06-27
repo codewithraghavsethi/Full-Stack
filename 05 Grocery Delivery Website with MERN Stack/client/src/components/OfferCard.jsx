5cb523cfdf36865d29745e991315628e09b161b0
codewithraghavsethi <codewithraghavsethi@gmail.com>
2025-06-27 22:03:42 +0530

    code

---
+++ 05 Grocery Delivery Website with MERN Stack/client/src/components/OfferCard.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const OfferCard = () => {
+  return (
+    <div>
+        <h1>Raghav Sethi</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Blanditiis magni optio consequuntur facilis odit minus ex, dignissimos suscipit aliquam, laudantium natus eum ipsa error. Eum pariatur illo aliquid quas ullam.</p>
+    </div>
+  )
+}
+
+export default OfferCard
