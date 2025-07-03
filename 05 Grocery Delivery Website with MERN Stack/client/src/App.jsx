9e4effe2f1ea62cefa2fa1f1fca6ccc1fd2e8c2b
codewithraghavsethi <codewithraghavsethi@gmail.com>
2025-07-03 09:42:57 +0530

    code

--- 05 Grocery Delivery Website with MERN Stack/client/src/App.jsx
+++ 05 Grocery Delivery Website with MERN Stack/client/src/App.jsx
@@ -4,13 +4,18 @@ import { Route, Routes, useLocation } from "react-router-dom";
 import Home from "./pages/Home.jsx";
 import { Toaster } from "react-hot-toast";
 import Footer from "./components/Footer.jsx";
+import { useAppContext } from "./context/AppContext.jsx";
+import Login from "./components/Login.jsx";
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
+  const {showUserLogin} = useAppContext()
 
   return (
     <div>
       { isSellerPath ? null : <Navbar /> } 
+      {showUserLogin ? <Login/> : null}
+
       <Toaster />
       <div
         className={`${isSellerPath ? "" : "px-6 md:px-16 lg:px-24 xl:px-32"} `}
