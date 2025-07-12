1a96849cfb3489b88ead6d6eaddbfe6a591527df
codewithraghavsethi <codewithraghavsethi@gmail.com>
2025-07-12 22:19:27 +0530

    code

---
+++ 05 Grocery Delivery Website with MERN Stack/client/src/components/Scl.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const Scl = () => {
+  return (
+    <div>
+        <h1>Raghav Sethi</h1>
+        <h1>Exam Date - 13/07/2025</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Debitis deserunt maxime cum sed placeat ipsam, consequatur est unde tempora non ratione velit eius ipsa amet modi doloribus nisi odit voluptates.</p>
+    </div>
+  )
+}
+
+export default Scl
