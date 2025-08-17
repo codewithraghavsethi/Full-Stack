c37e6a752d9f98e446a0917ce18b68551253ad8a
codewithraghavsethi <codewithraghavsethi@gmail.com>
2025-08-17 15:11:11 +0530

    code

--- 05 Grocery Delivery Website with MERN Stack/client/src/pages/AddAddress.jsx
+++ 05 Grocery Delivery Website with MERN Stack/client/src/pages/AddAddress.jsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import { assets } from '../assets/greencart_assets/assets'
 
 // Input Field Component
-const InputField = ({type, placeholder, name, handleChange, address})=>{
+const InputField = ({type, placeholder, name, handleChange, address})=>(
     <input className='w-full px-2 py-2.5 border border-gray-500/30 rounded outline-none text-gray-500 focus:border-primary transition' 
     type={type}
     placeholder={placeholder}
@@ -11,12 +11,13 @@ const InputField = ({type, placeholder, name, handleChange, address})=>{
     value={address[name]}
     required
     />
-}
+)
 
 
 const AddAddress = () => {
 
     const [address, setAddress] = useState({
+
         firstName: '',
         lastName: '',
         email: '',
@@ -32,7 +33,7 @@ const AddAddress = () => {
         const {name, value} = e.target;
 
         setAddress((prevAddress)=>({
-            ...AddAddress,
+            ...prevAddress,
             [name] : value,
         }))
         console.log(address);
