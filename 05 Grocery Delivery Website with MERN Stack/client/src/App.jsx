3bc1162b6c4263407d5c8f09c4796cf8c3746277
codewithraghavsethi <codewithraghavsethi@gmail.com>
2025-08-22 19:00:16 +0530

    code

--- 05 Grocery Delivery Website with MERN Stack/client/src/App.jsx
+++ 05 Grocery Delivery Website with MERN Stack/client/src/App.jsx
@@ -12,11 +12,13 @@ import ProductDetails from "./pages/ProductDetails.jsx";
 import Cart from "./pages/Cart.jsx";
 import AddAddress from "./pages/AddAddress.jsx";
 import MyOrders from "./pages/MyOrders.jsx";
+import SellerLogin from "./components/seller/SellerLogin.jsx";
+
 
 
 const App = () => {
   const isSellerPath = useLocation().pathname.includes("seller");
-  const {showUserLogin} = useAppContext()
+  const {showUserLogin, isSeller} = useAppContext()
 
   return (
     <div>
@@ -35,6 +37,8 @@ const App = () => {
           <Route path="/cart" element={<Cart/>} />
           <Route path="/add-address" element={<AddAddress/>} />
           <Route path="/my-orders" element={<MyOrders/>} />
+          <Route path="/seller" element={isSeller ? null : <SellerLogin/>}> 
+          </Route>
         </Routes>
       </div>
       {!isSellerPath && <Footer />}
